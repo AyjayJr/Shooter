e42fb8641e7460d13e0405a5d11c75f1d0eb4f4c
marco-lacal <marco.lacal@knights.ucf.edu>
2022-09-23 00:33:59 -0400

    More work on grapple
    So before, you could glitch the grapple by flicking your view to have the guntip be far from the grappleLocation and cause problems. NOW, the gun rotates to face the grappleLocation so gunTip cant be flicked and will always finish the while loop, then the gun will loop again to put it back to normal orientation. HOWEVER, high speeds have a chance to glitch the grapple, but that can be done later.

--- Assets/Scripts/Grapple.cs
+++ Assets/Scripts/Grapple.cs
@@ -1,3 +1,13 @@
+/*
+Currently I have implemented a scoogie ass / halfass fix to a bug where if you look around while grappling so far that gunTip doesnt come close enough to contactPoint, you wont
+disconnect. I temporarily fixed it right now by referencing the player script to manually change the sensitivity so you "shouldnt" be able to flick around and bug it out
+
+Another possible better longterm fix could be to have the gun rotate towards the contact point and "follow" the grapple line, that way theoretically it will never bug out since
+the gunTip will always be directed towards the contactPoint             IMPLEMENT THIS
+
+Nvm new bug: sometimes grapple glitches and stays on the smaller sensitivity
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,20 +19,22 @@ public class Grapple : MonoBehaviour
     [SerializeField] private Transform playerTransform;
     private Vector3 grappleLocation;
     [SerializeField] private Transform gunTip;
+    [SerializeField] private Transform _head;
+    //private CPMPlayer moveScript;
     public float grappleRange;
     //public float grappleSpeed;
     private bool isDeployed = false;
 
     void Awake()
     {
-        lr = GetComponentInChildren<LineRenderer>();
+        //moveScript = GetComponentInParent<CPMPlayer>();
 
-        Debug.Log(playerCamera.name + "   " + playerTransform.name);
+        lr = GetComponentInChildren<LineRenderer>();
     }
     
     void Update()
     {
-        if(Input.GetMouseButtonDown(1))
+        if(Input.GetMouseButtonDown(1) && !isDeployed)
         {
             ShootGrapple();
         }
@@ -50,17 +62,55 @@ public class Grapple : MonoBehaviour
 
         grappleLocation = hit.point;
 
+        //Loop to pull player to grapple location and have gun always look at the grapple location
         while(Vector3.Distance(gunTip.position, grappleLocation) >= 2f)
         {
-            //Had to increase the interpolation value because the gravity of the player made upward grapples not work (player too thicc)
-            playerTransform.position = Vector3.Lerp(playerTransform.position, grappleLocation, 0.1f);
+            //SMG looks at grapple point so gunTip never gets flicked around
+            transform.LookAt(grappleLocation);
+
+            playerTransform.position = Vector3.Lerp(playerTransform.position, grappleLocation, 0.12f);
 
             yield return null;
         }
 
-        //Debug.Log("Destination reached");
+        //Stop drawing line after being pulled
         isDeployed = false;
+
+        //Loop to reset the gun to face in the direction the player is facing
+        while(transform.rotation != playerCamera.rotation)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, playerCamera.rotation, 0.06f);
+
+            yield return null;
+        }
     }
+
+    // OLD IMPLEMENTATION WHERE I SLOWED DOWN SENSITIVITY SO THEY COULDNT FLICK AND GLITCH OUT GRAPPLE
+    // IEnumerator DeployGrapple(RaycastHit hit)
+    // {
+    //     float temp = moveScript.xMouseSensitivity;
+    //     moveScript.xMouseSensitivity = temp/3;
+    //     moveScript.yMouseSensitivity = temp/3;
+        
+    //     isDeployed = true;
+
+    //     grappleLocation = hit.point;
+
+    //     while(Vector3.Distance(gunTip.position, grappleLocation) >= 2f)
+    //     {
+    //         //playerCamera.LookAt(grappleLocation);
+
+    //         //Had to increase the interpolation value because the gravity of the player made upward grapples not work (player too thicc)
+    //         playerTransform.position = Vector3.Lerp(playerTransform.position, grappleLocation, 0.12f);
+    //         //playerCamera.LookAt(grappleLocation, Vector3.up);
+
+    //         yield return null;
+    //     }
+
+    //     moveScript.xMouseSensitivity = temp;
+    //     moveScript.yMouseSensitivity = temp;
+    //     isDeployed = false;
+    // }
 
     void DrawGrapple()
     {
