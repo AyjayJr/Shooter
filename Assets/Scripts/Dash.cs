ad6e746012d3bd83182bdd819f5def282c067e4e
marco-lacal <marco.lacal@knights.ucf.edu>
2022-09-29 00:04:45 -0400

    dash
    idk

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    private CPMPlayer script;
+
+    void Start()
+    {
+        script = GetComponent<CPMPlayer>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ActivateDash()
+    {
+        
+    }
+}
