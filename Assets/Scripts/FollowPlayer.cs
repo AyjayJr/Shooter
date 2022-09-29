3debd1fe8c2499bcc7b51c8abfbe1f9b72b9f389
AyjayJr <ayjayjr@hotmail.com>
2022-09-29 18:50:36 -0400

    deleted unused assets and scenes

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCam : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
 	 public Transform cameraPosition;
 
